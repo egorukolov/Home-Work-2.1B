bd5c0750350d3d4cdfd2cd862cd7228c23bdc0bc
Egor Ukolov <ukolovegor@yandex.ru>
2020-05-20 13:06:49 +0700

    Initial Commit

---
+++ Home Work 2.1B/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home Work 2.1B
+//
+//  Created by Egor Ukolov on 20.05.2020.
+//  Copyright © 2020 Egor Ukolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
