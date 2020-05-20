3da7089cb87cb362bc7a4728b6fb745065634139
Egor Ukolov <ukolovegor@yandex.ru>
2020-05-20 16:30:15 +0700

    Проработка логики программы

--- Home Work 2.1B/ViewController.swift
+++ Home Work 2.1B/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var startNextButtom: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSignalView.layer.cornerRadius = 60
+        yellowSignalView.layer.cornerRadius = 60
+        greenSignalView.layer.cornerRadius = 60
+        
+        redSignalView.alpha = 0.5
+        yellowSignalView.alpha = 0.5
+        greenSignalView.alpha = 0.5
     }
 
 
+    @IBAction func startButtomPressed() {
+        
+        startNextButtom.setTitle("Next", for: .normal)
+        
+        if redSignalView.alpha == 0.5 && yellowSignalView.alpha == 0.5 && greenSignalView.alpha == 0.5 {
+            redSignalView.alpha = 1
+        } else if redSignalView.alpha == 1 {
+            redSignalView.alpha = 0.5
+            yellowSignalView.alpha = 1
+        } else if yellowSignalView.alpha == 1 {
+            yellowSignalView.alpha = 0.5
+            greenSignalView.alpha = 1
+        } else if greenSignalView.alpha == 1  {
+            redSignalView.alpha = 1
+            greenSignalView.alpha = 0.5
+        }
+    }
 }
 
